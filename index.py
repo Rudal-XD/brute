f11e96c70e61b7b961c8ecc9073b805c753fcde0
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-10-31 09:30:55 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,25 @@
+import os,sys,system,json
+import rich 
+import json
+
+def banner():
+       print(f'''
+       pilih yang mau di jalankan:
+       1.youtube
+       2.whatsaap
+       3.exit
+       ''')
+       pilih.self()
+       def pilih(self):
+              if = input('choose:')
+              elif = '1' or '01'
+              r=get('xdg-open https://youtube.com/channel/UC5v_VjMTskJ6JjgxNLlXBCg')
+              else:
+              exit()
+              elif = '2' or '02'
+              r=get('xdg-open https://wa.me.0895386194665')
+              else:
+              exit()
+              elif = '3' or '03'
+              exit()
+           
