0c753b6e5699ffc7fcb7eafdccc0f7685b1bfdbd
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-10-31 09:35:44 +0700

    Update index.py

--- index.py
+++ index.py
@@ -12,14 +12,14 @@ def banner():
        pilih.self()
        def pilih(self):
               if input('choose:'):
-              elif = '1' or '01'
-              r=get('xdg-open https://youtube.com/channel/UC5v_VjMTskJ6JjgxNLlXBCg')
-              else:
-              exit()
-              elif = '2' or '02'
-              r=get('xdg-open https://wa.me.0895386194665')
-              else:
-              exit()
-              elif = '3' or '03'
-              exit()
+               elif = '1' or '01'
+                 r=get('xdg-open https://youtube.com/channel/UC5v_VjMTskJ6JjgxNLlXBCg')
+               else:
+                 exit()
+               elif = '2' or '02'
+                 r=get('xdg-open https://wa.me.0895386194665')
+               else:
+                 exit()
+               elif = '3' or '03'
+                 exit()
            
