2092935b87a99fcb620959f304421005057eb2d8
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 22:46:24 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-import os,time
+import os,time,requests
 
 try:
-  r=request.get('xdg-open https://wa.me/62895386194665')
+  r=requests.get('xdg-open https://wa.me/62895386194665')
 except ImportError:
   print('ada kesalahan fatal dalam script')
