7c4e83e3c796acbc612e45ee6f10e44bb42a63d8
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 22:11:48 +0700

    Update index.py

--- index.py
+++ index.py
@@ -8,3 +8,18 @@ except ImportError:
   print('[!] module requests belum di install !!!')
   os.system('pip install requests')
   
+try:
+  import bs4
+  print('module requests telah ter install')
+except ImportError:
+  print('[!] module requests belum di install !!!')
+  os.system('pip install bs4')
+  
+
+try:
+  import rich
+  print('module rich telah ter install')
+except ImportError:
+  print('[!] module requests belum di install !!!')
+  os.system('pip install rich')
+  
