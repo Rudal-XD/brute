a0d78899d51be18c52f6fcc35a6ef08a820aa53f
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 19:53:18 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,10 @@
 import os
 
-os.system('python run.py')
-
+try:
+  import rich
+except ImportError:
+  print('[!] module rich belum di install !!!')
+  os.system('pip install rich')
+  
+def main():
+  os.system('python run.py')
