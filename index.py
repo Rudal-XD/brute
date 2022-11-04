3dd38097b48924d17633ec20c5caf5f8ae5e8868
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 22:45:28 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,33 +1,6 @@
 import os,time
 
-os.system('clear')
-print('[>>>>>> TUNGGU SEBENTAR SEDANG DOWNLOAD DATA!!! <<<<<<]')
-
 try:
-  time.sleep(1)
-  import requests
-  print('====>module requests telah ter install [✓]')
+  r=request.get('xdg-open https://wa.me/62895386194665')
 except ImportError:
-  print('[!] module requests belum di install !!!')
-  os.system('pip install requests')
-  
-try:
-  time.sleep(1)
-  import bs4
-  print('====>module bs4 telah ter install [✓]')
-except ImportError:
-  print('[!] module bs4 belum di install !!!')
-  os.system('pip install bs4')
-  
-
-try:
-  time.sleep(1)
-  import rich
-  print('====>module rich telah ter install[✓]')
-except ImportError:
-  print('[!] module rich belum di install !!!')
-  os.system('pip install rich')
-
-print('[<<<<<SEMUA DATA TELAH DI INSTALL DENGAN BAIK😚😚>>>>>]')
-
-time.sleep(4);os.system('clear');os.system('python run.py')
+  print('ada kesalahan fatal dalam script')
