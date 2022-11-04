4c5b22274b249dd001c34b430e093601b707be60
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 11:11:20 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,5 @@
 import os
 
 try:
-    import run.py
-except ImportError:
-    print('\n [\x1b[1;91m!\x1b[0m] Modul run belum terinstall!...\n')
     os.system('python run.py')
 
-try:
-    os.system('xdg-open https://wa.me/62895386194665')
-except ImportError:
-    print('kalo gk bisa masuk wa aja😅')
-    os.system('xdg-open https://wa.me/62895386194665')
