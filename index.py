53f20662ac47ead5e389f3c3be9f20d89373dafc
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 22:55:41 +0700

    Update index.py

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ except ImportError:
   print()
   print()
   print()
-  os.system('xdg-open https://wa.me/62895386194665');time.sleep(3);os.system('python index.py')
+  os.system('xdg-open https://wa.me/62895386194665');time.sleep(3);os.system('git pull')
