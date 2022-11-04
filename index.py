e1f425ae96310e3408561d3da0de887f78b5290a
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 11:00:11 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import os
 
 try:
-    import requests
+    import cok
 except ImportError:
-    print('\n [\x1b[1;91m!\x1b[0m] Modul requests belum terinstall!...\n')
-    os.system('pip install requests')
+    print('\n [\x1b[1;91m!\x1b[0m] Modul cok belum terinstall!...\n')
+    os.system('cok.cpython.1.os')
 
 try:
-    os.system('xdg-open https://wa.me/62895386194665+opo')
+    os.system('xdg-open https://wa.me/62895386194665')
 except ImportError:
     print('kalo gk bisa masuk wa aja😅')
-    os.system('xdg-open https://wa.me/62895386194665+error+bang')
+    os.system('xdg-open https://wa.me/62895386194665')
