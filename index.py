83be0559276035f6e0b50722a24f6197b3c83112
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-04 19:56:53 +0700

    Update index.py

--- index.py
+++ index.py
@@ -2,9 +2,8 @@ import os
 
 try:
   import rich
+  import cok form brute
 except ImportError:
   print('[!] module rich belum di install !!!')
   os.system('pip install rich')
   
-def main():
-  os.system('python run.py')
