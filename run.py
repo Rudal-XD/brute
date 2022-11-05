274e16f6dfe4802f2ce6704890403d3e13687e77
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-06 06:34:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@ import os,time,sys
 os.system('clear')
 def jalan(self):
 	for wibu in self + "\n":
-		sys.stdout.write('TUNGGU SEBENTAR SEDANG MENG-INSTALL FILE YANG DIPERLUKAN')
+		sys.stdout.write('#########################################')
+                sys.stdout.write('#/n')
+                sys.stdout.write('#########################################')
 		sys.stdout.flush()
 		time.sleep(0.03)
 jalan('self')
