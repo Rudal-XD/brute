7f3680ecec5abeb9c335f00eca66f3aada4067b1
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-05 12:35:38 +0700

    Update index.py

--- index.py
+++ index.py
@@ -5,7 +5,19 @@ import requests
 try:
    print('male or female')
    print()
-   r=requests.get('https://google.com')
+   def __init__(self):
+        def main(self):
+            usna == input('choose:')
+             if usna in ['']:
+             print()
+             elif usna in ['1']
+                 try:
+                    r=requests.get('https://google.com')
+                 except IOError:
+                    print('error')
+                    exit('maaf script error')
+               else:
+                    exit('hahaha')   
 except IOError:
    print('jangan kosong kak')
    os.system('git pull')
