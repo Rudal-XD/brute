71252121d9a2959734f2e83fb48b690a528c1ff1
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-05 10:38:39 +0700

    Update index.py

--- index.py
+++ index.py
@@ -4,10 +4,36 @@ import rich
 import bs4
 import requests
 
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU,
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA YANG UDAH GAK PERAWAN :V
+J = '\033[38;2;255;127;0;1m' # ORANGE
 
 def banner():
      print(f'''
-     +++++++++++++++++++++++++++++++++++++++++++++
+╭━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━╮
+┃╭┈─────────────⩵꙰ཱི࿐
+┃╰── ⏤͟͟͞GOOD BYE ──➤ ↶↷
+╰━━•›ꪶ ཻུ۪۪ꦽꦼ̷⸙ ━ ━ ━ ━ ꪶ ཻུ۪۪ꦽꦼ̷⸙‹•━━͙✩̣̣̣̣
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+╭━━•›〘 STATUS 〙
+│➳ LEAVING FROM
+│➳ ${metadata.subject}
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•⩵꙰ཱི࿐
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+╭━━•›〘 SUBSCRIBE 〙
+│➳ Channel YouTube
+│➳ FARIZ MODS
+│➳ https://youtube.com/channel/UCmfGOnP_M0ucKN8Y5Y4yBOQ
+╰━ ━ ━ ━ ━ ━ ━ ━ ━ ━•⩵꙰ཱི࿐
+ ▬▭▬▭▬ ✦✧✦ ▬▭▬▭▬
+
+© FARIZ V5
      ''')
 
 def menu():
