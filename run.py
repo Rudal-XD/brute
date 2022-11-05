26c19b485501e14bbcde832a4b3180bef1077491
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-06 06:35:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 import os,time,sys
 os.system('clear')
 def jalan(self):
-	for wibu in self + "\n":
-	       sys.stdout.write('#########################################')
-               sys.stdout.write('#/n')
-               sys.stdout.write('#########################################')
+	for # in self + "\n":
+	        sys.stdout.write('#########################################')
+                       sys.stdout.write('#')
+                sys.stdout.write('#########################################')
 		sys.stdout.flush()
 		time.sleep(0.03)
 jalan('self')
