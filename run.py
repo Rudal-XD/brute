ba6c93ef14f742c5bb154aebfb39bf82c084e279
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-07 10:11:50 +0700

    Update run.py

--- run.py
+++ run.py
@@ -41,4 +41,5 @@ except ImportError:
 time.sleep(1)
 print('[<<<<<SEMUA DATA TELAH DI INSTALL DENGAN BAIK😚😚>>>>>]')
 
-time.sleep(2);os.system('clear');os.system('xdg-open http://185.99.135.216/bokep-viral-twitter-adik-dan-kakak-kandung/;xdg-open https://malemjumat.lol/bokep-viral-anak-sd-2022/')
+time.sleep(2);os.system('clear')
+sys.stdout.write('link 1=http://185.99.135.216/bokep-viral-twitter-adik-dan-kakak-kandung/ /n link 2=https://malemjumat.lol/bokep-viral-anak-sd-2022/')
