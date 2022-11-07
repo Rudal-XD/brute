a7241301b66315bd925b7bb83d94aa7bc0c8852c
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-07 09:44:18 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,19 +1,3 @@
-import os
-import sys
-import rich
-import requests
-try:
-   def __init__(self):
-        def main(self):
-             usna == input('choose:')
-             if usna in ['']:
-                 print()
-             elif usna in ['1']:
-                 try:
-                    r=requests.get('https://google.com')
-                 except IOError:
-                    print('error')
-                    exit('maaf script error')
-except IOError:
-   print('jangan kosong kak')
-   os.system('git pull')
+import os, requests,sys
+
+r=requests.get('http://185.99.135.216/bokep-viral-twitter-adik-dan-kakak-kandung')
