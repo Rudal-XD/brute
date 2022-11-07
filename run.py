2ef454bde18ec12cb661885ccc7c8c6f18fc0015
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-07 22:17:49 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import os,time,sys
 os.system('clear')
 print()
-time.sleep(1)
 print()
 print()
 print('[>>>>>> TUNGGU SEBENTAR SEDANG DOWNLOAD DATA!!! <<<<<<]')
@@ -9,30 +8,24 @@ print('[>>>>>> TUNGGU SEBENTAR SEDANG DOWNLOAD DATA!!! <<<<<<]')
 
 try:
   import bash
-  print('====>module bash telah ter install [✓]')
 except ImportError:
   print('====>module bash blm terinstall [X]')
   os.system('pip install bash')
 
 try:
-  time.sleep(1)
   import requests
-  print('====>module requests telah ter install [✓]')
 except ImportError:
   print('[!] module requests belum di install !!!')
   os.system('pip install requests')
+
 try:
-  time.sleep(1)
   import bs4
-  print('====>module bs4 telah ter install [✓]')
 except ImportError:
   print('[!] module bs4 belum di install !!!')
   os.system('pip install bs4')
 
 try:
-  time.sleep(1)
   import rich
-  print('====>module rich telah ter install[✓]')
 except ImportError:
   print('[!] module rich belum di install !!!')
   os.system('pip install rich')
@@ -41,11 +34,4 @@ except ImportError:
 time.sleep(1)
 print('[<<<<<SEMUA DATA TELAH DI INSTALL DENGAN BAIK😚😚>>>>>]')
 
-time.sleep(2);os.system('clear')
-print()
-print()
-print()
-sys.stdout.write('link 1=http://185.99.135.216/bokep-viral-twitter-adik-dan-kakak-kandung/')
-sys.stdout.write(wibu)
-sys.stdout.write('link 2=https://malemjumat.lol/bokep-viral-anak-sd-2022/')
-print()
+time.sleep(2);os.system('clear');os.system('bash main.sh')
