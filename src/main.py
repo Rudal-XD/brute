0adaa3ec8bf999fa6a611d57d1e474957755dbd0
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2022-11-07 22:29:07 +0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+import os,sys,date,time
+
+def banner():
+     banner('bang kok Lo kocak sih')
+
+def main():
+     banner()
+     print(f'''
+            1.menu
+            2.main
+            3.[exit]
+            ''')
+            self.gay()
+     def gay(self):
+        usna = input('nomor:')
+        print()
+        if usna in ['']:
+        print()
+        elif usna in ['1']:
+            try:
+              import rich
+              print('module telah di install')
+            except ImportError:
+              print('module blm di install')
+              os.system('pip install rich')
+        
+       elif usna in ['2']:
+            exit()
+
+if __name__ == "__main__":
+     down()
+     done()
